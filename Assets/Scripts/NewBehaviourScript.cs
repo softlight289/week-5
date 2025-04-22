a015f1c7dfcf38626815b037fb9ab4853632663d
softlight289 <myrzanurlina289@gmail.com>
2025-04-22 09:15:03 +0800

    Update NewBehaviourScript.cs

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello WOrld");   
+        Debug.Log("Hello World keep smiling! :D");   
     }
 
     // Update is called once per frame
