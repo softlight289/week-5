2c3e261b13fb8260a6588ce8e46ae2c94b84432e
Softlight289 <87424811+softlight289@users.noreply.github.com>
2025-04-22 11:38:35 +0800

    Coins Rotating Script

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+
+    public GameObject ObjCenter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(ObjCenter.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
